86c5833c7fedd8dd160f1b9b2fbc322b301feed8
Lukas Frei <lukas.frei@zuehlke.cim>
2018-02-24 11:29:43 +0100

    Add logging for echo method to track calls to backend

--- src/main/java/com/redbeard/timer/EchoController.java
+++ src/main/java/com/redbeard/timer/EchoController.java
@@ -1,6 +1,8 @@
 package com.redbeard.timer;
 
 import com.redbeard.timer.domain.ServerStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EchoController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @CrossOrigin
     @GetMapping("/echo")
     public ServerStatus getEcho() {
+        log.info("echo method called");
         return ServerStatus.create("Timer Backend running");
     }
 }
