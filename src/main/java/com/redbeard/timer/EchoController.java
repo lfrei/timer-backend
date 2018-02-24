9ffbbd010fbd5bbfe4122d789786ef192c73bb1c
Lukas Frei <lukas.frei@zuehlke.cim>
2018-02-24 11:09:39 +0100

    Allow echo to be called

--- src/main/java/com/redbeard/timer/EchoController.java
+++ src/main/java/com/redbeard/timer/EchoController.java
@@ -1,11 +1,13 @@
 package com.redbeard.timer;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
 
+    @CrossOrigin
     @GetMapping("/echo")
     public String getEcho() {
         return "Timer backend running";
